cd0e64096c0f882bbc4d508c0a4dfe9fe03f85a9
sscumac <66644788+sscumac@users.noreply.github.com>
2024-08-12 05:17:54 +0200

    Update FlyingTextBlock.vue

--- src/components/FlyingTextBlock.vue
+++ src/components/FlyingTextBlock.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white flex-col w-full p-4 opacity-90 flex justify-center items-center lg:text-2xl max-w-screen-lg mx-auto"
+    class="bg-white flex-col w-full p-4 opacity-90 flex justify-center items-center lg:text-xl max-w-screen-lg mx-auto"
   >
     <div style="line-height: 1.5" v-html="text"></div>
     <img v-if="first" src="/src/assets/icons/down.svg" alt="scroll-down" width="80" height="80" />
